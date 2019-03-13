912c2825c90ebf1381ce3f90a7f1714ed4186304
Julien Clément <julienclement22@gmail.com>
2019-03-13 20:06:40 +0100

    TestRoom - Stop player when hitting a platform + add "enemies"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     private bool isMoving;
     public float PlayerSpeed = 100.0f;
     private Rigidbody2D rigidbody2D;
+    private Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         isMoving = false;
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -41,6 +43,21 @@ public class PlayerController : MonoBehaviour
         Vector2 velocity = rigidbody2D.velocity;
         if(velocity.x == 0 && velocity.y == 0)
         {
+            isMoving = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Contains("Platform"))
+        {
+            isMoving = false;
+            rigidbody2D.velocity = new Vector2(0,0);
+        }
+        else if (col.gameObject.name.Contains("Enemy"))
+        {
+            transform.position = startPos;
+            rigidbody2D.velocity = new Vector2(0,0);
             isMoving = false;
         }
     }
