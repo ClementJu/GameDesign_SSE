3d2db46f81a80aa573f34b462fbbdc1d7ec40848
Johan Jobin <26439579+JobinJohan@users.noreply.github.com>
2019-03-29 15:48:07 +0100

    Moveable ennemy, default values added

--- Assets/Scripts/moveEnnemy.cs
+++ Assets/Scripts/moveEnnemy.cs
@@ -14,9 +14,10 @@ public class moveEnnemy : MonoBehaviour
     void Start()
     {
         initialPosition = transform.position;
-        int speed = 50;
+        int speed = 5;
         direction = 1;
         maxDist = 2;
+        directionOfMove = new Vector3(1, 1, 0);
     }
 
     // Update is called once per frame
