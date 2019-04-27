5bbc66b9bf342f2d486d232ff2810e3a396e94c1
Marom <romain.flac@gmail.com>
2019-04-27 11:21:42 +0200

    Create a global game manager

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour
 {
+    public void newGame()
+    {
+        GameObject.Find("GameManager").GetComponent<gamemanager>().LoadScene("Scenes/1_level");
+    }
     public void LoadScene(int scene)
     {
         SceneManager.LoadScene(scene);
